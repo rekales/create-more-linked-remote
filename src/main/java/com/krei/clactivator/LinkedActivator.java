dcde27e6e55124c4109f04b3454a8151c312a1bd
Krei <50932886+rekales@users.noreply.github.com>
2025-05-06 16:52:18 +0800

    setup fix

---
+++ src/main/java/com/krei/clactivator/LinkedActivator.java
@@ -0,0 +1,27 @@
+package com.krei.clactivator;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(LinkedActivator.MODID)
+public class LinkedActivator {
+    public static final String MODID = "createlinkedactivator";
+    @SuppressWarnings("unused")
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
+    public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.registerSimpleItem("linkedactivator");
+
+    public LinkedActivator(IEventBus modEventBus, ModContainer modContainer) {
+        ITEMS.register(modEventBus);
+    }
+}
